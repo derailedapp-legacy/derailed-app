72c7b91a23349212d287f56661007c54f5271dc0
Vincent <vincentbusiness55@gmail.com>
2022-10-08 13:07:46 +0000

    Update 19 files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Derailed App
-This is what the derailed app will become. For now though, it's just a testing ground to see how things work.
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ['./src/pages/*'],
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
